724e8b994b547ee627661698114b456cebe8f24d
igorthedci <foumart@ukr.net>
2018-12-07 18:54:57 +0200

    oxwall

--- test_comment_delete.py
+++ test_comment_delete.py
@@ -1,37 +1,26 @@
 import time
 
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from page_objects import locators
 
 
 def test_comment_delete(app, login_user):
     message_box = app.driver.find_elements(*locators.NEWSFEED_MESSAGES_CONTENT)[0]
-    time.sleep(2)
+    # time.sleep(2)
     message_box.click()
     more_button = message_box.find_element(*locators.NEWSFEED_MESSAGES_MORE)
-    time.sleep(2)
+    # time.sleep(2)
     more_button.click()
     # delete_button = message_box.find_element(*locators.NEWSFEED_MESSAGES_DELETE)
     delete_button = message_box.find_element(*locators.NEWSFEED_MESSAGES_DELETE)
     delete_button.click()
-    time.sleep(2)
+    # time.sleep(2)
     try:
-        wait.until(EC.alert_is_present(),
-                                    'Timed out waiting for PA creation ' +
-                                    'confirmation popup to appear.')
-
-    alert = browser.switch_to.alert
-    alert.accept()
-    print("alert accepted")
-except TimeoutException:
-    print("no alert")
-    try:
-        delete_button.click()
-    except UnexpectedAlertPresentException:
-    time.sleep(3)
-
-    try:
-        self.wait.until(EC.alert_is_present())
+        app.wait.until(EC.alert_is_present())
+        alert = app.driver.switch_to.alert
+        alert.accept()
         return True
     except TimeoutException:
-        self.actions.send_keys(Keys.ESCAPE).perform()
+        print("no alert")
         return False
