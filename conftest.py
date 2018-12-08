37232bc2670b06629a9ff32d8ef74265dc60f3e9
igorthedci <foumart@ukr.net>
2018-12-08 15:42:32 +0200

    class User

--- conftest.py
+++ conftest.py
@@ -15,4 +15,12 @@ def login_user(app):
     yield
     app.logout('admin')
 
+@pytest.fixture()
+def admin_user_old(app):
+    return {'username': 'admin', 'password': 'Adm1n'}
+
+@pytest.fixture
+def admin_user():
+    return User(username='admin', password='Adm1n')
+
 
