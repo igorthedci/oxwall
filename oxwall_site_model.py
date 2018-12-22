8d6e24fcef5d1173702c91a794fb013658a09bcb
igorthedci <foumart@ukr.net>
2018-12-22 13:38:25 +0200

    oxwall. add locators directory

--- oxwall_site_model.py
+++ oxwall_site_model.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
-from page_object import locators
+from page_objects import locators
 import time
 
 
