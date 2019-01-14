0c79ffdcdb0bdc36fb329599b89d40bec396dff1
igorthedci <foumart@ukr.net>
2019-01-14 16:39:07 +0200

    join page @elpiankova, please review join.page no email verification no captcha

--- page_objects/join_page.py
+++ page_objects/join_page.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 
@@ -8,103 +11,107 @@ class JoinPage(InternalPage):
 
     TITLE_LOCATOR = (By.CSS_SELECTOR, "h1.ow_ic_file")
 
-    USERNAME_TITLE = (By.XPATH, '//td[contains(@class,"ow_alt1")]/label[text()="Username"]')
-    USERNAME_FIELD = (By.XPATH, '//td[contains(@class,"ow_alt1")]/label[text()="Username"]/../..//input')
-    EMAIL_FIELD = (By.XPATH, '//td[contains(@class,"ow_alt1")]/label[text()="Email"]/../..//input')
+    USERNAME_TITLE = (By.XPATH, '//label[text()="Username"]')
+    USERNAME_FIELD = (By.XPATH, '//label[text()="Username"]/../..//input')
+    EMAIL_FIELD = (By.XPATH, '//label[text()="Email"]/../..//input')
+    PASSWORD_FIELD = (By.XPATH, '//label[text()="Password"]/../..//input')
+    REPEATPASSWORD_FIELD = (By.XPATH, '//label[text()="Repeat password"]/../..//input')
+    REALNAME_FIELD = (By.XPATH, '//label[text()="Real name"]/../..//input')
 
-    PASSWORD_FIELD = (By.CSS_SELECTOR, "input[name='password']")
-    REPEATPASSWORD_FIELD = (By.CSS_SELECTOR, "input[name='repeatPassword']")
-    REALNAME_FIELD = (By.CSS_SELECTOR, "tr:nth-of-type(15) input")
-    GENDER_MALE = (By.CSS_SELECTOR, 'tr:nth-of-type(18) li>input[value="1"]')
-    GENDER_FEMALE = (By.CSS_SELECTOR, 'tr:nth-of-type(18) li>input[value="2"]')
-    BD_DAY_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(24) select[name*="day"]')
-    BD_MONTH_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(24) select[name*="month"]')
-    BD_YEAR_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(24) select[name*="year"]')
-    FOR_MALE_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(35) li>input[value="1"]')
-    FOR_FEMALE_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(35) li>input[value="2"]')
-    FOR_FUN_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(35) li>input[value="1"]')
-    FOR_FRIENDSHIP_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(35) li>input[value="8"]')
-    FOR_DATING_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(35) li>input[value="4"]')
-    FOR_WHATEVER_FIELD = (By.CSS_SELECTOR, 'tr:nth-of-type(35) li>input[value="2"]')
-    MUSIC_INTEREST = (By.CSS_SELECTOR, 'tr:nth-of-type(44) textarea')
-    BOOK_INTEREST = (By.CSS_SELECTOR, 'tr:nth-of-type(50) textarea')
+    GENDER_MALE = (By.XPATH, '//label[text()="Gender"]/../..//label[text()="Male"]/..//input')
+    GENDER_FEMALE = (By.XPATH, '//label[text()="Gender"]/../..//label[text()="Female"]/..//input')
+
+    BD_DAY_FIELD = (By.CSS_SELECTOR, 'select[name*="day"]')
+    BD_MONTH_FIELD = (By.CSS_SELECTOR, 'select[name*="month"]')
+    BD_YEAR_FIELD = (By.CSS_SELECTOR, 'select[name*="year"]')
+
+    FOR_MALE_FIELD = (By.XPATH, '//label[text()="Looking for"]/../..//label[text()="Male"]/..//input')
+    FOR_FEMALE_FIELD = (By.XPATH, '//label[text()="Looking for"]/../..//label[text()="Female"]/..//input')
+
+    FOR_FUN_FIELD = (By.XPATH, '//label[text()="Here for"]/../..//label[text()="Fun"]/..//input')
+    FOR_FRIENDSHIP_FIELD = (By.XPATH, '//label[text()="Here for"]/../..//label[text()="Friendship"]/..//input')
+    FOR_DATING_FIELD = (By.XPATH, '//label[text()="Here for"]/../..//label[text()="Dating"]/..//input')
+    FOR_WHATEVER_FIELD = (By.XPATH, '//label[text()="Here for"]/../..//label[text()="Whatever"]/..//input')
+
+    MUSIC_INTEREST = (By.XPATH, '//label[text()="Music"]/../..//textarea')
+    BOOK_INTEREST = (By.XPATH, '//label[text()="Favorite books"]/../..//textarea')
     # USER_PHOTO = (By.CSS_SELECTOR, 'for the future')
 
-    JOIN_BUTTON = (By.NAME, "joinSubmit")
+    JOIN_BUTTON = (By.CSS_SELECTOR, 'span.ow_ic_submit')
 
     # TODO Add all elements and actions that you have in Main Page
 
     @property
     def username_field(self):
-        return self.find_visible_element(self.USERNAME_FIELD)
+        return self.select_valid_element(self.USERNAME_FIELD)
 
     @property
     def email_field(self):
-        return self.find_visible_element(self.EMAIL_FIELD)
+        return self.select_valid_element(self.EMAIL_FIELD)
 
     @property
     def password_field(self):
-        return self.find_visible_element(self.PASSWORD_FIELD)
+        return self.select_valid_element(self.PASSWORD_FIELD)
 
     @property
     def repeatpassword_field(self):
-        return self.find_visible_element(self.REPEATPASSWORD_FIELD)
+        return self.select_valid_element(self.REPEATPASSWORD_FIELD)
 
     @property
     def realname_field(self):
-        return self.find_visible_element(self.REALNAME_FIELD)
+        return self.select_valid_element(self.REALNAME_FIELD)
 
     @property
     def gender_male_field(self):
-        return self.find_visible_element(self.GENDER_MALE)
+        return self.select_valid_element(self.GENDER_MALE)
 
     @property
     def gender_female_field(self):
-        return self.find_visible_element(self.GENDER_FEMALE)
+        return self.select_valid_element(self.GENDER_FEMALE)
 
     @property
     def birthday_day_field(self):
-        return self.find_visible_element(self.BD_DAY_FIELD)
+        return self.select_valid_element(self.BD_DAY_FIELD)
 
     @property
     def birthday_month_field(self):
-        return self.find_visible_element(self.BD_MONTH_FIELD)
+        return self.select_valid_element(self.BD_MONTH_FIELD)
 
     @property
     def birthday_year_field(self):
-        return self.find_visible_element(self.BD_YEAR_FIELD)
+        return self.select_valid_element(self.BD_YEAR_FIELD)
 
     @property
     def looking_for_male_field(self):
-        return self.find_visible_element(self.FOR_MALE_FIELD)
+        return self.select_valid_element(self.FOR_MALE_FIELD)
 
     @property
     def looking_for_female_field(self):
-        return self.find_visible_element(self.FOR_FEMALE_FIELD)
+        return self.select_valid_element(self.FOR_FEMALE_FIELD)
 
     @property
     def here_for_fun_field(self):
-        return self.find_visible_element(self.FOR_FUN_FIELD)
+        return self.select_valid_element(self.FOR_FUN_FIELD)
 
     @property
     def here_for_friendship_field(self):
-        return self.find_visible_element(self.FOR_FRIENDSHIP_FIELD)
+        return self.select_valid_element(self.FOR_FRIENDSHIP_FIELD)
 
     @property
     def here_for_dating_field(self):
-        return self.find_visible_element(self.FOR_DATING_FIELD)
+        return self.select_valid_element(self.FOR_DATING_FIELD)
 
     @property
     def here_for_whatever_field(self):
-        return self.find_visible_element(self.FOR_WHATEVER_FIELD)
+        return self.select_valid_element(self.FOR_WHATEVER_FIELD)
 
     @property
     def music_interest(self):
-        return self.find_visible_element(self.MUSIC_INTEREST)
+        return self.select_valid_element(self.MUSIC_INTEREST)
 
     @property
     def book_interest(self):
-        return self.find_visible_element(self.BOOK_INTEREST)
+        return self.select_valid_element(self.BOOK_INTEREST)
 
     @property
     def join_button(self):
@@ -129,16 +136,19 @@ class JoinPage(InternalPage):
         self.realname_field.send_keys(realname)
 
     def input_gender(self, gender):
-        gender_field = self.gender_male_field if gender.lower() in ['m', 'male', '1'] else self.gender_female_field
+        gender_field = self.gender_female_field if gender.lower() in ['f', 'female', '2'] else self.gender_male_field
         gender_field.click()
 
     def input_birthday(self, birthday):  # BIRTHDAY
-        select = Select(self.birthday_day_field)
-        select.select_by_visible_text(birthday[0])
-        select = Select(self.birthday_month_field)
-        select.select_by_visible_text(birthday[1])
-        select = Select(self.birthday_year_field)
-        select.select_by_visible_text(birthday[2])
+        locator = self.birthday_day_field
+        locator.click()
+        Select(locator).select_by_visible_text(str(birthday[0]))
+        locator = self.birthday_month_field
+        locator.click()
+        Select(locator).select_by_visible_text(str(birthday[1]))
+        locator = self.birthday_year_field
+        locator.click()
+        Select(locator).select_by_visible_text(str(birthday[2]))
 
     def input_looking_for(self, looking_for):
         if looking_for[0]:
@@ -166,11 +176,11 @@ class JoinPage(InternalPage):
 
     def submit_form(self):  # JOIN BUTTON
         self.join_button.click()
-        locator = self.LOGIN_BACKGROUND
-        try:
-            self.wait.until(invisibility_of_element_located(locator))
-        except TimeoutException:
-            raise NoSuchElementException("No element by locator {}".format(locator))
+        # locator = self.LOGIN_BACKGROUND
+        # try:
+        #     self.wait.until(invisibility_of_element_located(locator))
+        # except TimeoutException:
+        #     raise NoSuchElementException("No element by locator {}".format(locator))
 
 
 if __name__ == "__main__":
@@ -185,5 +195,23 @@ if __name__ == "__main__":
 
     join_page = JoinPage(driver)
 
-    join_page.input_username("tested")
-    join_page.input_email("qq@qq.qq")
+    random_string = ''.join(random.sample('abcdefghijklmnopqrstuvwxyz', 14))
+
+    join_page.input_username(random_string)
+    email = 'qq' + random_string + '@gmail.com'
+    join_page.input_email(email)
+    join_page.input_password("pass1234")
+    join_page.input_realname("The Tester")
+
+    join_page.input_gender("Female")
+
+    join_page.input_birthday([1, "Jan", 1991])
+    # join_page.input_birthday([31, "Dec", 1930])
+
+    join_page.input_looking_for([1, 1])
+    join_page.input_here_for([1, 1, 0, 1])
+
+    join_page.input_music_interest("Classic")
+    join_page.input_book_interest("Python Guides")
+
+    join_page.submit_form()
--- page_objects/page.py
+++ page_objects/page.py
@@ -1,7 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import ActionChains
-from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located, element_to_be_clickable
 
 
 class Page:
@@ -20,29 +20,15 @@ class Page:
     def find_visible_element(self, locator):
         return self.wait.until(visibility_of_element_located(locator), "No visible element with {}".format(locator))
 
+    def find_clickable_element(self, locator):
+        return self.wait.until(element_to_be_clickable(locator), "No clickable element with {}".format(locator))
+
+    def select_valid_element(self, locator):
+        locators = self.driver.find_elements(*locator)
+        for locator in locators:
+            if locator.size["height"] > 0:
+                return locator
+
     @property
     def current_url(self):
         return self.driver.current_url
-
-    def find_clickable_element(self, locator):
-        return self.wait.until(clickability_of_element_located(locator))
-
-
-class InputTextElement:
-    def __init__(self, webelement):
-        self.element = webelement
-
-    def input(self, text):
-        """Sets the text to the value supplied"""
-        self.element.clear()
-        self.element.send_keys(text)
-
-    @property
-    def placeholder(self):
-        """Gets the text of the specified object"""
-        return self.element.get_attribute("placeholder")
-
-    @property
-    def text(self):
-        """Gets the text of the specified object"""
-        return self.element.get_attribute("value")
